3069ad29732a9f6bdf3774d2fcfdfb1a8424b4c8
MiguelDev <miguelmartinezm@outlook.es>
2024-12-14 19:43:34 +0100

    rxjs section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import {MatTab, MatTabGroup} from "@angular/material/tabs";
 import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
 import {MatDialog} from "@angular/material/dialog";
 import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
+import {catchError, from, interval, startWith, throwError} from "rxjs";
 import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
 import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 import { openEditCourseDialog } from '../edit-course-dialog/edit-course-dialog.component';
@@ -30,13 +30,13 @@ export class HomeComponent {
     coursesService = inject(CoursesService);
     dialog = inject(MatDialog);
 
-    courses = signal<Course[]>([]);
+    #courses = signal<Course[]>([]);
     begginerCourses = computed(() => {
-        const courses = this.courses();
+        const courses = this.#courses();
         return courses.filter(course => course.category === 'BEGINNER');
     });
     advancedCourses = computed(() => {
-        const courses = this.courses();
+        const courses = this.#courses();
         return courses.filter(course => course.category === 'ADVANCED');
     });
     loadingService = inject(LoadingService);
@@ -45,18 +45,21 @@ export class HomeComponent {
         read: ElementRef
     });
 
+    
+
 
     constructor() {
+
         effect(() => {
-            console.log(this.begginersList());
+            // console.log(this.begginersList());
         })
 
         effect(() => {
-            console.log('Begginers: ', this.begginerCourses());
-            console.log('Advanced: ', this.advancedCourses());
+            // console.log('Begginers: ', this.begginerCourses());
+            // console.log('Advanced: ', this.advancedCourses());
         });
 
-        this.loadCourses().then(() => console.log('All courses loaded', this.courses()));
+        this.loadCourses().then(() => console.log('All courses loaded', this.#courses()));
         
     }
     
@@ -64,7 +67,7 @@ export class HomeComponent {
     async loadCourses() {
         try {
             const courses = await this.coursesService.loadAllCourses();
-            this.courses.set(courses.sort(sortCoursesBySeqNo));
+            this.#courses.set(courses.sort(sortCoursesBySeqNo));
             
         } catch (error) {
             this.messageService.showMessage('Error loading courses!', 'error');
@@ -73,20 +76,20 @@ export class HomeComponent {
 
 
     onCourseUpdated(updaetdCourse: Course) {
-        const courses = this.courses();
+        const courses = this.#courses();
         const newCourses = courses.map(course => (
             course.id === updaetdCourse.id ? updaetdCourse : course
         ));
-        this.courses.set(newCourses);
+        this.#courses.set(newCourses);
         this.messageService.showMessage('Updated course', 'success');
     }
 
    async onCourseDeleted(courseId: string) {
         try {
             await this.coursesService.deleteCourse(courseId);
-            const courses = this.courses();
+            const courses = this.#courses();
             const newCourses = courses.filter(course => course?.id !== courseId);
-            this.courses.set(newCourses);
+            this.#courses.set(newCourses);
         } catch (error) {
             this.messageService.showMessage('Error deleting course', 'error');
         }
@@ -101,11 +104,62 @@ export class HomeComponent {
             return;
         }
         const newCourses = [
-            ...this.courses(),
+            ...this.#courses(),
             newCourse
         ];
-        this.courses.set(newCourses);
+        this.#courses.set(newCourses);
         this.messageService.showMessage('New course created', 'success');
+    }
+
+    injector = inject(Injector);
+
+    onToObservable() {
+        const courses$ = toObservable(this.#courses, {
+            injector: this.injector
+        });
+        courses$.subscribe(courses => console.log(courses));
+
+        //------
+        const numbers = signal(0);
+        numbers.set(1);
+        numbers.set(2);
+        numbers.set(3);
+        const numbers$ = toObservable(numbers, {
+            injector: this.injector
+        });
+        numbers.set(4);
+        numbers$.subscribe(value => console.log(value));
+        numbers.set(5);
+    }
+
+    // courses$ = from(this.coursesService.loadAllCourses());
+    onToSignalExample() {
+        const courses$ = from(this.coursesService.loadAllCourses()).pipe(
+            catchError(err => {
+                console.log(err);
+                throw err;
+            })
+        );
+        const courses = toSignal(courses$, {
+            injector: this.injector
+        });
+    
+        effect(() => {
+            console.log(courses())
+        }, {
+            injector: this.injector
+        });
+
+        //--------
+        const number$ = interval(1000);
+        const numbers = toSignal(number$, {
+            injector: this.injector,
+        });
+        effect(() => {
+            // console.log(numbers())
+        }, {
+            injector: this.injector
+        });
     }
 
 
